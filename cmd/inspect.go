5b6cf9e312f0a949726694e7c6de5d5662de29c7
Yeung Yiu Hung <hung@shoplineapp.com>
2019-08-23 17:08:03 +0800

    feat(alfread): add alfred output

--- cmd/inspect.go
+++ cmd/inspect.go
@@ -7,12 +7,24 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
+	"github.com/drgrib/alfred"
+
 	jira "github.com/andygrunwald/go-jira"
 	helpers "github.com/darkcl/jira/helpers"
 	models "github.com/darkcl/jira/models"
 )
 
 var shouldOpen bool
+var shouldOutputAlfred bool
+
+func consoleLog(format string, a ...interface{}) {
+	if shouldOutputAlfred {
+		return
+	}
+
+	// Hack around
+	fmt.Printf(format, a...)
+}
 
 // inspectCmd represents the inspect command
 var inspectCmd = &cobra.Command{
@@ -41,20 +53,36 @@ to quickly create a Cobra application.`,
 		}
 
 		if len(ticketNumber) == 0 {
-			fmt.Printf("Jira ticket not provided.\n")
+			consoleLog("Jira ticket not provided.\n")
 			return
+		}
+
+		browserURL := fmt.Sprintf("%s/browse/%s", viper.GetString("host"), ticketNumber)
+
+		consoleLog("Issue URL: %s\n", browserURL)
+		if shouldOpen {
+			helpers.OpenBrowser(browserURL)
+		}
+
+		if shouldOutputAlfred {
+			alfred.Add(alfred.Item{
+				Title:    ticketNumber,
+				Subtitle: "Issue Key",
+				Arg:      browserURL,
+				UID:      "issue.Key",
+			})
 		}
 
 		client, _ := jira.NewClient(tp.Client(), viper.GetString("host"))
 		issue, _, err := client.Issue.Get(ticketNumber, nil)
 
 		if err != nil {
-			fmt.Printf("\nerror: %v\n", err)
+			consoleLog("\nerror: %v\n", err)
 			return
 		}
-		fmt.Printf("[%s](%s) %s\n", issue.Key, issue.ID, issue.Fields.Summary)
-		fmt.Printf("Status: %s \n", issue.Fields.Status.Name)
-		fmt.Printf("Assignee: %s \n", issue.Fields.Assignee.DisplayName)
+		consoleLog("[%s](%s) %s\n", issue.Key, issue.ID, issue.Fields.Summary)
+		consoleLog("Status: %s \n", issue.Fields.Status.Name)
+		consoleLog("Assignee: %s \n", issue.Fields.Assignee.DisplayName)
 
 		prEndpoint := fmt.Sprintf("rest/dev-status/1.0/issue/detail?issueId=%s&applicationType=bitbucket&dataType=pullrequest", issue.ID)
 
@@ -66,38 +94,53 @@ to quickly create a Cobra application.`,
 			panic(err)
 		}
 
-		fmt.Println()
+		consoleLog("\n")
 
 		for _, detail := range devStatus.Detail {
 			for _, branch := range detail.Branches {
-				fmt.Printf("git checkout %s\t [%s]\n", branch.Name, branch.Repository.Name)
+				consoleLog("git checkout %s\t [%s]\n", branch.Name, branch.Repository.Name)
 			}
 
-			fmt.Println()
+			consoleLog("\n")
 
 			for _, pr := range detail.PullRequests {
-				fmt.Printf("Pull Request: %s [%s]\n", pr.Name, pr.Status)
-				fmt.Printf("Pull Request URL: %s \n\n", pr.URL)
+				consoleLog("Pull Request: %s [%s]\n", pr.Name, pr.Status)
+				consoleLog("Pull Request URL: %s \n\n", pr.URL)
 
 				if shouldOpen && pr.Status == "OPEN" {
 					helpers.OpenBrowser(pr.URL)
 				}
+
+				if shouldOutputAlfred && pr.Status == "OPEN" {
+					alfred.Add(alfred.Item{
+						Title:    issue.Key,
+						Subtitle: "Pull Request",
+						Arg:      pr.URL,
+						UID:      "pull.request",
+					})
+				}
 			}
 		}
 
-		browserURL := fmt.Sprintf("%s/browse/%s", viper.GetString("host"), issue.Key)
-
-		fmt.Printf("Issue URL: %s\n", browserURL)
-		if shouldOpen {
-			helpers.OpenBrowser(browserURL)
-		}
-
-		fmt.Printf("================================\n\n")
+		consoleLog("================================\n\n")
 		epic := helpers.GetEpicFromIssue(issue.ID, client)
-		epicURL := fmt.Sprintf("%s/browse/%s", viper.GetString("host"), epic.Key)
-		fmt.Printf("Epic URL: %s\n", epicURL)
-		if shouldOpen {
-			helpers.OpenBrowser(epicURL)
+		if epic != nil {
+			epicURL := fmt.Sprintf("%s/browse/%s", viper.GetString("host"), epic.Key)
+			consoleLog("Epic URL: %s\n", epicURL)
+			if shouldOpen {
+				helpers.OpenBrowser(epicURL)
+			}
+			if shouldOutputAlfred {
+				alfred.Add(alfred.Item{
+					Title:    issue.Key,
+					Subtitle: "Epic Key",
+					Arg:      epicURL,
+					UID:      "epic.Key",
+				})
+			}
+		}
+		if shouldOutputAlfred {
+			alfred.Run()
 		}
 	},
 }
@@ -105,7 +148,7 @@ to quickly create a Cobra application.`,
 func init() {
 	rootCmd.AddCommand(inspectCmd)
 	rootCmd.PersistentFlags().BoolVarP(&shouldOpen, "open", "o", false, "Open in browser")
-
+	rootCmd.PersistentFlags().BoolVarP(&shouldOutputAlfred, "alfred", "a", false, "Output in alfred")
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/andygrunwald/go-jira v1.10.0
+	github.com/drgrib/alfred v0.0.0-20180713231015-cbcb1a4b1a30
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/viper v1.4.0
